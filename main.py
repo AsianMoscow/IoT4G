55750d6f5e236955e289526293bf66b4f8772af4
AsianMoscow <theavsv@gmail.com>
2025-11-29 21:55:57 +0300

    8

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     root.title("IoT4G — Auto Update Test")
     root.geometry("300x200")
 
-    label = tk.Label(root, text="Hello world! 7", font=("Arial", 20))
+    label = tk.Label(root, text="Hello world! 8", font=("Arial", 20))
     label.pack(expand=True)
 
     root.mainloop()
